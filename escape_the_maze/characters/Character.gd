8ad2471ad1bd62922bafa5000742c987217fca09
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2023-10-16 19:34:36 -0300

    Added player, enemy, pickups

--- escape_the_maze/characters/Character.gd
+++ escape_the_maze/characters/Character.gd
@@ -1,2 +1,38 @@
 extends Area2D
 
+export (int) var speed
+
+var tile_size = 64
+var can_move = true
+var facing = Vector2.RIGHT
+
+var moves = {	"down"	: Vector2.DOWN,
+				"left"	: Vector2.LEFT,
+				"right"	: Vector2.RIGHT, 
+				"up"	: Vector2.UP}
+
+onready var raycasts = {"down"	: $RayCastDown,
+						"left"	: $RayCastLeft,
+						"right"	: $RayCastRight,
+						"up"	: $RayCastUp}
+
+func move(dir) -> bool:
+	$AnimationPlayer.playback_speed = speed
+	facing = dir
+	if raycasts[facing].is_colliding():
+		return false
+	can_move = false
+	$AnimationPlayer.play(facing)
+	$MoveTween.interpolate_property(self, 
+									"position", 
+									position,
+									position + moves[facing] * tile_size, 
+									1.0/speed, 
+									Tween.TRANS_SINE, 
+									Tween.EASE_IN_OUT)
+	$MoveTween.start()
+	return true
+
+
+func _on_MoveTween_tween_completed(_object, _key):
+	can_move = true
